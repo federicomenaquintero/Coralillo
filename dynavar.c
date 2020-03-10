d462c2636d8eed298ec4718c811ae6969a28e5c1
Cristian Gotchev <christumer@gmail.com>
2020-03-09 23:43:28 -0600

    finished new_type_var functions.

--- dynavar.c
+++ dynavar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dynavar.h"
 
 
@@ -9,16 +10,49 @@ Var new_int_var(int x)
     var.type = "int";
     var.nxt = NULL;
 
-    var.data = (int*)malloc(sizeof(int));
+    var.data = (int*)calloc(1, sizeof(int));
     *(int*)var.data = x;
+    
     return var;
 }
 
-// Lists
+Var new_string_var(char *x)
+{
+    Var var;
+    var.type = "string";
+    var.nxt = NULL;
+
+    var.data = (char*)calloc(strlen(x), sizeof(x)); //Test
+    strcpy((char*)var.data, x);
+    
+    return var;
+}
+
+// Npi de si esto funciona
+Var new_bool_var(bool x)
+{
+    Var var;
+    var.type = "boolean";
+    var.nxt = NULL;
+
+    var.data = (bool*)calloc(1, sizeof(x));
+    *(bool*)var.data = x;
+    
+    return var;
+}
+
+
 int main()
 {
-    Var a = new_int_var(10);
-    printf("%d\n", *(int*)a.data);
-    printf("%s\n", a.type);
+    int x = 1;
+    printf("%ld\n", sizeof(x));
+    
+    Var s = new_string_var("holaaa");
+    printf("%s -> %ld\n", (char*)s.data, strlen(s.data));
+
+    Var b = new_bool_var(false);
+    if (*(bool*)b.data == true) printf("true\n");
+    else if(*(bool*)b.data == false) printf("false\n");
+
     return 0;
 }
