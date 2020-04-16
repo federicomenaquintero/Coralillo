654d00116045d9f59be9221fca538172a04bcec6
Bulagro <christumer@gmail.com>
2020-04-16 15:32:47 -0500

    updated lexer tests, removed unnecesary files.

--- tests/lexer.rs
+++ tests/lexer.rs
@@ -1,155 +1,95 @@
-/*
-Tokens are a struct which will always come like:
-Token {
-    name // The type of token this is, can only be one of the
-        constants defined below.
-    val // How it appears in your text file, unless specified otherwise
-}
-*/
-
-const NUMBER: &'static str = "NUMBER";
-const NAME: &'static str = "NAME";
-// Val here is the line number it ends
-const NEWLINE: &'static str = "NEWLINE";
-// Val in the two below is how many spaces or tabs before the first token
-const INDENT: &'static str = "INDENT";
-const UNINDENT: &'static str = "UNINDENT";
-
-const KEYWORD: &'static str = "KEYWORD";
-// You won't find a Token.name = KEYWORDS, instead this one is used to identify keywords
-// for Token.name = KEYWORD
-const KEYWORDS: &[&'static str] = &["if", "let", "for"];
+const NAME: &str = "NAME";
+const NUMBER: &str = "NUMBER";
+const INDENT: &str = "INDENT";
+const UNINDENT: &str = "UNINDENT";
+const NWLN: &str = "NWLN";
+const KEYWORD: &str = "KEYWORD";
+const KEYWORDS: [&str; 3] = ["if", "else", "for"];
 
 pub struct Token {
     name: &'static str,
-    val: String,
+    value: String,
 }
-
 impl Token {
-    pub fn to_string(&self) -> String {
-        format!("Token({}, {})", self.name, self.val)
+    pub fn new(name: &'static str, value: String) -> Token {
+        Token { name, value }
     }
 }
 
-pub fn lexer(path: &str) -> Vec<Token> {
-    fn is_number(ch: char) -> bool {
-        return '0' <= ch && ch <= '9';
-    }
-
-    fn is_nam(ch: char) -> bool {
-        return ('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z') || (ch == '_');
-    }
-
-    fn check_indent(output: &mut Vec<Token>, spaces: u16) {
-        for i in 0..output.len() {
-            let j = output.len() - i - 1;
-            match output.get(j) {
-                Some(tkn) if tkn.name == INDENT || tkn.name == UNINDENT => {
-                    let aux: u16 = tkn.val.parse::<u16>().unwrap();
-                    if spaces > aux {
-                        output.push(Token {
-                            name: INDENT,
-                            val: spaces.to_string(),
-                        });
-                    } else if spaces < aux {
-                        output.push(Token {
-                            name: UNINDENT,
-                            val: spaces.to_string(),
-                        });
-                    }
-                    return;
-                }
-
-                Some(_) | None => {}
-            }
+pub fn lexer(file: Vec<u8>) -> Result<Vec<Token>, String> {
+    let mut _aux = Vec::<char>::new();
+    let file_slice: &[char] = {
+        for byte in file {
+            _aux.push(byte as char)
         }
-    }
+        _aux.as_ref()
+    };
+    let _aux = 0;
 
-    let mut output = vec![Token {
-        name: INDENT,
-        val: "0".to_string(),
-    }];
-    let file = std::fs::read(path).expect(&format!("Unable to read file {}", path));
+    let mut output = Vec::<Token>::new();
     let mut i: usize = 0;
-    let mut ln: u128 = 1;
-    let mut spaces: u16 = 0;
-    loop {
-        match file.get(i) {
-            Some(byte) if is_number(*byte as char) => {
-                check_indent(&mut output, spaces);
-                let mut buffer = (*byte as char).to_string();
-                loop {
-                    match file.get(i + 1) {
-                        Some(b) if is_number(*b as char) => {
-                            buffer = format!("{}{}", buffer, *b as char);
-                            i += 1;
-                        }
-                        Some(_) | None => break,
-                    }
-                }
-                output.push(Token {
-                    name: NUMBER,
-                    val: buffer,
-                });
-
-                i += 1;
-            }
-
-            Some(byte) if is_nam(*byte as char) => {
-                check_indent(&mut output, spaces);
-                let mut buffer = (*byte as char).to_string();
-                loop {
-                    match file.get(i + 1) {
-                        Some(b) if is_nam(*b as char) || is_number(*b as char) => {
-                            buffer = format!("{}{}", buffer, *b as char);
-                            i += 1;
-                        }
-                        Some(_) | None => break,
-                    }
-                }
-                if KEYWORDS.iter().any(|&aux| aux == buffer) {
-                    output.push(Token {
-                        name: KEYWORD,
-                        val: buffer,
-                    });
+    let mut line_counter = 0_u128;
+    let mut ind_counter = 0_u16;
+    let mut last_ind = 0_u16;
+    while i < file_slice.len() {
+        // Indentation
+        if let Some(tkn) = output.last() {
+            if tkn.name == NWLN {
+                if file_slice[i] == ' ' || file_slice[i] == '\t' {
+                    ind_counter += 1;
+                    i += 1;
+                    continue;
                 } else {
-                    output.push(Token {
-                        name: NAME,
-                        val: buffer,
-                    });
-                }
-
-                i += 1;
-            }
-
-            Some(byte) if *byte as char == '\n' => {
-                check_indent(&mut output, spaces);
-                output.push(Token {
-                    name: NEWLINE,
-                    val: ln.to_string(),
-                });
-                i += 1;
-                ln += 1;
-                spaces = 0;
-            }
-
-            Some(byte) if *byte == ' ' as u8 || *byte as char == '\t' => {
-                match output.last() {
-                    Some(tkn) if tkn.name == NEWLINE => {
-                        spaces += 1;
+                    use std::cmp::Ordering::*;
+                    match ind_counter.cmp(&last_ind) {
+                        Greater => output.push(Token::new(INDENT, ind_counter.to_string())),
+                        Less => output.push(Token::new(UNINDENT, ind_counter.to_string())),
+                        Equal => {}
                     }
-                    Some(_) | None => {}
+                    last_ind = ind_counter;
+                    ind_counter = 0;
                 }
-                i += 1;
+            }
+        }
+
+        // Names and keywords
+        if file_slice[i].is_alphabetic() {
+            let mut value = String::new();
+            while file_slice[i].is_alphanumeric() {
+                value.push(file_slice[i]);
+                i += 1
             }
 
-            Some(_) => {
-                i += 1;
+            if KEYWORDS.contains(&value.as_str()) {
+                output.push(Token::new(KEYWORD, value))
+            } else {
+                output.push(Token::new(NAME, value))
             }
-            None => (break),
         }
+
+        // Numbers
+        if file_slice[i].is_digit(10) {
+            let mut value = String::new();
+            while file_slice[i].is_digit(10) {
+                value.push(file_slice[i]);
+                i += 1
+            }
+
+            output.push(Token::new(NUMBER, value))
+        }
+
+        // Single character tokens
+        match file_slice[i] {
+            '\r' | ' ' | '\t' => {}
+            '\n' => {
+                line_counter += 1;
+                output.push(Token::new(NWLN, line_counter.to_string()))
+            }
+            unexpected => return Err(format!("Lexer Error: Unexpected Token {}", unexpected)),
+        }
+
+        i += 1
     }
 
-    output.remove(0);
-    return output;
+    Ok(output)
 }
--- tests/lextertest.py
+++ tests/lextertest.py
@@ -1,46 +1,92 @@
 # Vairiable names must start with a letter or _ and are alowed to have any visvible chatacter in them that's not an operator.
 # A name ends when a space or an operator is found.
-from string import digits, ascii_letters
 
-DIGITS = tuple(digits)
+DIGITS = tuple('0123456789')
 OPERATORS = (
     '+', '-', '/', '*',                 # Arithmetic
     '=', '+=', '-=', '*=', '/=',        # Assignment
     '!', '&&', '||',                    # Logic
     '==', '!=', '>', '<', '>=', '<=',   # Comparative
     '[', ']', '.',                      # Information retrieval
+    '->',                               # Miscelaneous
     )
 AGROUPATION_SYMBOLS = ('(', ')')
-SEPARATORS = (',', ':', ';')
+SEPARATORS = (',', ':', ';', ' ')
+KEYWORDS = (
+    'let', 'fn', 'context',
+    'for', 'while', 'loop',
+    'continue', 'break',
+    'return'
+)
 
 
-def unintrusive_split(line:str):
-    expressions = []
-    e_begin = 0
+def separate_line_by_separators(line:str):
+    separated_line = []
     inside_parenthesis = False
+    new_separation = ""
 
     i = 0
     while i < len(line):
-        inside_parenthesis = not inside_parenthesis if line[i] == '\"' or line[i] == '\'' else inside_parenthesis
+        inside_parenthesis = not inside_parenthesis if line[i] == '\'' else inside_parenthesis
         
-        if not inside_parenthesis:
-            if line[i] == ' ' and e_begin != i:
-                expressions.append(line[e_begin:i])
-                e_begin = i+1
-            
-            if line[i] in AGROUPATION_SYMBOLS or line[i] in OPERATORS or line[i] in SEPARATORS:
-                if e_begin == i:
-                    expressions.append(line[i])
-                    e_begin = i+1
-                else:
-                    expressions.append(line[e_begin:i])
-                    e_begin = i
+        if line[i] in SEPARATORS and not inside_parenthesis:
+            if new_separation: separated_line.append(new_separation)
+            if line[i] != ' ': separated_line.append(line[i])
+            new_separation = ""
+        else:
+            new_separation += line[i]
         
         i += 1
 
-    expressions.append(line[e_begin:i])
-    return expressions
+    separated_line.append(new_separation)
+    return separated_line
+
+def separate_line_by_strings(space_separated_line:list):
+    if type(space_separated_line) != list:
+        raise ValueError(f'Function separate_line_by_strings expects a list, given: {type(space_separated_line)}')
 
 
-operation = "(11)+[-(2121212121)] + \'Hola, me llamo Cris\') a(1, 2, 3)"
-print(unintrusive_split(operation))
+    string_separated_expressions = []
+    quote_count = 0
+    inside_parenthesis = False
+
+    for line_to_separate in space_separated_line:
+        while True:
+            quote_pos = line_to_separate.find('\'')
+
+            if quote_pos == -1: break
+            
+            separated_slice = line_to_separate[:quote_pos]
+            if inside_parenthesis: separated_slice = f'\'{separated_slice}\''
+            if separated_slice: string_separated_expressions.append(separated_slice)
+            
+            inside_parenthesis = not inside_parenthesis
+            quote_count += 1
+
+            line_to_separate = line_to_separate[quote_pos+1:]
+        
+        string_separated_expressions.append(line_to_separate)
+
+        if inside_parenthesis:
+            raise ValueError("Uneven quote count.")
+
+    return string_separated_expressions
+
+def separate_line_by_operators(string_separated_line:str):
+    pass
+
+
+def split_elements(line:str):
+
+    # Separate by spaces ------------------------------------------- #
+    line_separated_by_separators = separate_line_by_separators(line)
+    # Separate by strings ------------------------------------------ #
+    line_separated_by_strings = separate_line_by_strings(line_separated_by_separators)
+    # -------------------------------------------------------------- #
+
+    # print(line_separated_by_spaces)
+    print(line_separated_by_strings)
+    pass
+
+line = "(11)-(\'212\'12\'12\'121) + \'Hola, me llamo Cris\'awdnajdn   a -> (1, 2,s 3)"
+split_elements(line)
