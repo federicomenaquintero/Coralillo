230edb3a2d09d04734851c61714ea2352fcde2c9
Federico Mena Quintero <federico@gnome.org>
2020-04-18 18:18:54 -0500

    Tipo de error para Token

--- tests/lexer.py
+++ tests/lexer.py
@@ -1,6 +1,10 @@
 import unittest
 import enum
 
+from string import digits
+
+CARACTERES_BUENOS = tuple(digits)
+
 class TipoToken(enum.Enum):
     Numero = 0
     Oper_Mas = 1
@@ -9,13 +13,21 @@ class TipoToken(enum.Enum):
     Oper_Div = 4
     Paren_Izq = 5
     Paren_Der = 6
+    Error = 7
     
 class Token:
-    def __init__(self, tipo: TipoToken):
-        self.tipo = tipo
-        
     def __eq__(self, other):
         return self.tipo == other.tipo
+
+    def simple(self, tipo):
+        self.tipo = tipo
+        return self
+
+    def error(self, posicion, mensaje):
+        self.tipo = TipoToken.Error
+        self.error_pos = posicion
+        self.error_msg = mensaje
+        return self
 
 def tokeniza(input_: str):
     if len(input_) == 0:
@@ -27,12 +39,17 @@ def tokeniza(input_: str):
         caracter = input_[cursor:cursor + 1]
 
         if caracter == '+':
-            tokens.append(Token(TipoToken.Oper_Mas))
+            tokens.append(Token().simple(TipoToken.Oper_Mas))
         elif caracter == '-':
-            tokens.append(Token(TipoToken.Oper_Menos))
+            tokens.append(Token().simple(TipoToken.Oper_Menos))
+
         elif caracter == ' ':
             cursor += 1
             continue
+
+        elif caracter not in CARACTERES_BUENOS:
+            tokens.append(Token().error(cursor, "unexpected token"))
+            return tokens
 
         cursor += 1
 
@@ -43,12 +60,22 @@ class Pruebas(unittest.TestCase):
         self.assertEqual(tokeniza(""), [])
 
     def test_tokeniza_un_operador(self):
-        self.assertEqual(tokeniza("+"), [ Token(TipoToken.Oper_Mas) ])
+        self.assertEqual(tokeniza("+"), [ Token().simple(TipoToken.Oper_Mas) ])
 
     def test_tokeniza_dos_operadores(self):
         self.assertEqual(tokeniza("+ -"), [ 
-            Token(TipoToken.Oper_Mas),
-            Token(TipoToken.Oper_Menos)
+            Token().simple(TipoToken.Oper_Mas),
+            Token().simple(TipoToken.Oper_Menos)
         ])
+        
+    def test_encuentra_token_no_esperado(self):
+        tokens = tokeniza("+hola")
+        self.assertEqual(tokens, [ 
+            Token().simple(TipoToken.Oper_Mas), 
+            Token().simple(TipoToken.Error) 
+        ])
+        self.assertEqual(tokens[1].error_pos, 1)
+        self.assertEqual(tokens[1].error_msg, "unexpected token")
+
 
 unittest.main()
