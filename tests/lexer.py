6698f938ed9051b7ae12ebddc14a62d0398fce82
Federico Mena Quintero <federico@gnome.org>
2020-04-18 17:33:53 -0500

    Prueba de tokenizar un operador +

--- tests/lexer.py
+++ tests/lexer.py
@@ -14,8 +14,21 @@ def tokeniza(input_: str):
     if len(input_) == 0:
         return []
 
+    cursor = 0
+    tokens = []
+    while cursor < len(input_):
+        if input_[cursor:cursor + 1] == '+':
+            tokens.append(TipoToken.Oper_Mas)
+
+        cursor += 1
+
+    return tokens
+        
 class Pruebas(unittest.TestCase):
     def test_no_hay_tokens(self):
         self.assertEqual(tokeniza(""), [])
-        
+
+    def test_tokeniza_un_operador(self):
+        self.assertEqual(tokeniza("+"), [ TipoToken.Oper_Mas ])
+
 unittest.main()
